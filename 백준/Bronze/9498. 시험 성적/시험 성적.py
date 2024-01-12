020f15b2c08a57717edee2794eec6e4b7b049dd5
yunsejin <100738553+yunsejin@users.noreply.github.com>
2024-01-12 13:22:22 +0900

    [Bronze V] Title: 시험 성적, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/9498. 시험 성적/시험 성적.py
+++ 백준/Bronze/9498. 시험 성적/시험 성적.py
@@ -1,14 +1,14 @@
 score = int(input())
 
-if 100 >= score >= 90:
-    grade = 'A'
-elif 90 > score >= 80:
-    grade = 'B'
-elif 80 > score >= 70:
-    grade = 'C'
-elif 70 > score >= 60:
-    grade = 'D'
-else:
-    grade = 'F'
+int(score)
 
-print(grade)
+if 100 >= score >= 90:
+    print("A")
+elif 90 > score >= 80:
+    print("B")
+elif 80 > score >= 70:
+    print("C")
+elif 70 > score >= 60:
+    print("D")
+else:
+    print("F")
