4221b7c7aefe7326db1a70e7db7870b5d794a07c
yunsejin <100738553+yunsejin@users.noreply.github.com>
2024-01-12 10:28:21 +0900

    [Bronze V] Title: 사칙연산, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.py
@@ -0,0 +1,17 @@
+# 두 자연수 A와 B를 입력받기
+A, B = map(int, input().split())
+
+# A + B 출력
+print(A + B)
+
+# A - B 출력
+print(A - B)
+
+# A * B 출력
+print(A * B)
+
+# A / B (몫) 출력
+print(A // B)
+
+# A % B (나머지) 출력
+print(A % B)
