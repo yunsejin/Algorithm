694daf78c56a18e5eb8a43a9557ec33532a17f0d
yunsejin <100738553+yunsejin@users.noreply.github.com>
2024-01-15 01:51:04 +0900

    [Bronze V] Title: A+B - 3, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/10950. A＋B － 3/A＋B － 3.py
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.py
@@ -1,5 +1,9 @@
 a = int(input())
 
+results = []
 for i in range(1, a + 1):
     b, c = map(int, input().split())
-    print(b + c, end=" ")
+    results.append(b + c)
+
+for result in results:
+    print(result)
