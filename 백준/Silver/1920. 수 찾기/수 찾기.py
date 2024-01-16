5ce488b1ef0d87467479f85b7477345a28983d80
yunsejin <100738553+yunsejin@users.noreply.github.com>
2024-01-16 17:11:15 +0900

    [Silver IV] Title: 수 찾기, Time: 172 ms, Memory: 50380 KB -BaekjoonHub

---
+++ 백준/Silver/1920. 수 찾기/수 찾기.py
@@ -0,0 +1,10 @@
+N = int(input())
+A = set(map(int, input().split()))
+M = int(input())
+arr = list(map(int, input().split()))
+
+for num in arr:
+    if num in A:
+        print(1)
+    else:
+        print(0)
