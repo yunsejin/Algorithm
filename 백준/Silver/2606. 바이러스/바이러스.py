8ed7abccfa689562b20ef7f174f3846ee8825ffc
yunsejin <100738553+yunsejin@users.noreply.github.com>
2024-01-20 19:29:44 +0900

    [Silver III] Title: 바이러스, Time: 48 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.py
+++ 백준/Silver/2606. 바이러스/바이러스.py
@@ -2,19 +2,18 @@ n=int(input())
 v=int(input())
 graph = [[] for i in range(n+1)]
 visited=[0]*(n+1)
-
 for i in range(v):
     a,b=map(int,input().split())
     graph[a]+=[b]
     graph[b]+=[a]
-    
-def dfs(v):
-    stack=[v]
-    while stack:
-        v=stack.pop()
-        if visited[v]==0:
-            visited[v]=1
-            stack+=graph[v]
-
-dfs(1)
+def bfs(v):
+    queue=[v]
+    visited[v]=1
+    while queue:
+        v=queue.pop(0)
+        for nx in graph[v]:
+            if visited[nx]==0:
+                visited[nx]=1
+                queue.append(nx)
+bfs(1)
 print(sum(visited)-1)
