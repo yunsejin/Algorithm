b7fc60b8ebd296a3d82fae73229d8c1a817f6a28
yunsejin <100738553+yunsejin@users.noreply.github.com>
2024-01-20 19:40:50 +0900

    [Silver III] Title: 바이러스, Time: 48 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.py
+++ 백준/Silver/2606. 바이러스/바이러스.py
@@ -6,14 +6,11 @@ for i in range(v):
     a,b=map(int,input().split())
     graph[a]+=[b]
     graph[b]+=[a]
-def bfs(v):
-    queue=[v]
+def dfs(v):
     visited[v]=1
-    while queue:
-        v=queue.pop(0)
-        for nx in graph[v]:
-            if visited[nx]==0:
-                visited[nx]=1
-                queue.append(nx)
-bfs(1)
+    for nx in graph[v]:
+        if visited[nx]==0:
+            dfs(nx)
+
+dfs(1)
 print(sum(visited)-1)
