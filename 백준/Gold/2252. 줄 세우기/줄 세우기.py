577fbd8cb238c9d2d0ee67903c6599f49aee77ee
yunsejin <100738553+yunsejin@users.noreply.github.com>
2024-01-23 18:40:11 +0900

    [Gold III] Title: 줄 세우기, Time: 4180 ms, Memory: 38752 KB -BaekjoonHub

--- 백준/Gold/2252. 줄 세우기/줄 세우기.py
+++ 백준/Gold/2252. 줄 세우기/줄 세우기.py
@@ -1,29 +1,21 @@
-import sys
 from collections import deque
-input = sys.stdin.readline
+n,m=map(int,input().split())
+g=[[] for _ in range(n+1)]
+indegree=[0]*(n+1)
 
-def topological_sort(graph, indegree):
-    que = deque(node for node in range(1, len(indegree)) if indegree[node] == 0)
-                        
-    while que:
-        node = que.popleft()
-        print(node, end=' ')
-        for next_node in graph[node]:
-            indegree[next_node] -= 1
-            if indegree[next_node] == 0:
-                que.append(next_node)
-                  
-def main():
-    v, e = map(int, input().split())
-    graph = [[] for _ in range(v + 1)]
-    indegree = [0] * (v + 1)
-    
-    for _ in range(e):
-        a, b = map(int, input().split())
-        graph[a] += [b]
-        indegree[b] += 1
+for i in range(m):
+    a,b=map(int,input().split())
+    g[a].append(b)
+    indegree[b]+=1
 
-    topological_sort(graph, indegree)
-            
-if __name__ == '__main__':
-    main()
+q=deque()
+for i in range(1,n+1):
+    if indegree[i]==0:
+        q.append(i)
+while q:
+    now=q.popleft()
+    print(now,end=" ")
+    for next in g[now]:
+        indegree[next]-=1
+        if indegree[next]==0:
+            q.append(next)
