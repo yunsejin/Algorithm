6fe3b8d53d75ffe23e000a7a2893823452fea83b
yunsejin <100738553+yunsejin@users.noreply.github.com>
2024-01-24 19:27:56 +0900

    [Silver I] Title: 미로 탐색, Time: 80 ms, Memory: 34036 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.py
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.py
@@ -1,20 +1,17 @@
 import sys
 from collections import deque
-
-input = sys.stdin.readline
-n, m = map(int, input().split())
-g = [list(map(int, input().rstrip())) for _ in range(n)]
-dis = [(-1, 0), (1, 0), (0, -1), (0, 1)]
-
-def bfs(x, y):
-    q = deque([(x, y)])
+input=sys.stdin.readline
+n,m=map(int,input().split())
+g=[list(map(int,input().rstrip())) for _ in range(n)]
+dis=[(-1,0),(1,0),(0,-1),(0,1)]
+def bfs(x,y):
+    q=deque([(x,y)])
     while q:
-        x, y = q.popleft()
-        for dx, dy in dis:
-            nx, ny = x + dx, y + dy
-            if 0 <= nx < n and 0 <= ny < m and g[nx][ny] == 1:
-                g[nx][ny] = g[x][y] + 1
-                q.append((nx, ny))
+        x,y=q.popleft()
+        for dx,dy in dis:
+            nx,ny=x+dx,y+dy
+            if 0<=nx<n and 0<=ny<m and g[nx][ny]==1:
+                g[nx][ny]=g[x][y]+1
+                q.append((nx,ny))
     return g[-1][-1]
-
-print(bfs(0, 0))
+print(bfs(0,0))
