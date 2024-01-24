7b80ae5864577839184d7f401c6a2a3e4fa4255f
yunsejin <100738553+yunsejin@users.noreply.github.com>
2024-01-24 19:18:44 +0900

    [Silver I] Title: 미로 탐색, Time: 148 ms, Memory: 114076 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.py
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.py
@@ -2,37 +2,19 @@ import sys
 from collections import deque
 
 input = sys.stdin.readline
-
 n, m = map(int, input().split())
-graph = []
-for _ in range(n):
-    graph.append(list(map(int,input().rstrip())))
+g = [list(map(int, input().rstrip())) for _ in range(n)]
+dis = [(-1, 0), (1, 0), (0, -1), (0, 1)]
 
-dx = [-1,1,0,0]
-dy = [0,0,-1,1]
+def bfs(x, y):
+    q = deque([(x, y)])
+    while q:
+        x, y = q.popleft()
+        for dx, dy in dis:
+            nx, ny = x + dx, y + dy
+            if 0 <= nx < n and 0 <= ny < m and g[nx][ny] == 1:
+                g[nx][ny] = g[x][y] + 1
+                q.append((nx, ny))
+    return g[-1][-1]
 
-def bfs(x,y):
-
-    queue = deque()
-    queue.append((x,y))
-
-    while queue:
-        x,y = queue.popleft()
-
-        for i in range(4):
-            nx = x+dx[i]
-            ny = y+dy[i]
-
-            if nx<0 or nx>=n or ny<0 or ny>=m:
-                continue
-
-            if graph[nx][ny]==0:
-                continue
-
-            if graph[nx][ny]==1:
-                graph[nx][ny]=graph[x][y]+1
-                queue.append((nx,ny))
-
-    return graph[n-1][m-1]
-
-print(bfs(0,0))
+print(bfs(0, 0))
