3efbd2653b94ad0964f5238cd036d6ea5c41d960
yunsejin <100738553+yunsejin@users.noreply.github.com>
2024-01-27 17:20:12 +0900

    [Gold IV] Title: LCS 2, Time: 600 ms, Memory: 482120 KB -BaekjoonHub

---
+++ 백준/Gold/9252. LCS 2/LCS 2.py
@@ -0,0 +1,17 @@
+str1 = [''] + list(input().rstrip())
+str2 = [''] + list(input().rstrip())
+
+dp = [[''] * len(str2) for _ in range(len(str1))]
+
+for i in range(1, len(str1)):
+    for j in range(1, len(str2)):
+        if str1[i] == str2[j]:
+            dp[i][j] = dp[i-1][j-1] + str1[i]
+        else:
+            if len(dp[i-1][j]) >= len(dp[i][j-1]):
+                dp[i][j] = dp[i-1][j]
+            else:
+                dp[i][j] = dp[i][j-1]
+
+print(len(dp[-1][-1]))
+print(dp[-1][-1])
