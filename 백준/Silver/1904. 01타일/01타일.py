c1e1f7472eedb7f375069d6b654b6b3fbdea6f98
yunsejin <100738553+yunsejin@users.noreply.github.com>
2024-01-28 23:14:49 +0900

    [Silver III] Title: 01타일, Time: 112 ms, Memory: 109240 KB -BaekjoonHub

--- 백준/Silver/1904. 01타일/01타일.py
+++ 백준/Silver/1904. 01타일/01타일.py
@@ -1,6 +1,5 @@
-n = int(input())
-dp = [0,1,2]
-
-for i in range(3, n+1):
-    dp.append((dp[i-1] + dp[i-2])%15746)
-print(dp[n])
+n=int(input())%94488
+a,b=1,1
+for i in range(n-1):
+ a,b=b,(a+b)%15746
+print(b)
