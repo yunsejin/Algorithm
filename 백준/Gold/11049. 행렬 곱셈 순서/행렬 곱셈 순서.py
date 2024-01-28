75e864cde9625d697d7ed379ad971a7a75a259b8
yunsejin <100738553+yunsejin@users.noreply.github.com>
2024-01-28 23:56:36 +0900

    [Gold III] Title: 행렬 곱셈 순서, Time: 572 ms, Memory: 112676 KB -BaekjoonHub

--- 백준/Gold/11049. 행렬 곱셈 순서/행렬 곱셈 순서.py
+++ 백준/Gold/11049. 행렬 곱셈 순서/행렬 곱셈 순서.py
@@ -1,11 +1,24 @@
-n = int(input())
-mat = [tuple(map(int, input().split())) for i in range(n)]
+import sys
+input = sys.stdin.readline
 
-dp = [[0]*n for i in range(n)]
-for cnt in range(n-1):
-    for i in range(n-1-cnt):
-        j = i+cnt+1
-        dp[i][j] = 2**31
-        for k in range(i, j):
-            dp[i][j] = min(dp[i][j], dp[i][k] + dp[k+1][j] + mat[i][0]*mat[k][1]*mat[j][1])
-print(dp[0][-1])
+def matrix_chain_order(p):
+    n = len(p) - 1
+    dp = [[0] * len(p) for _ in range(len(p))]
+
+    for length in range(2, n + 1):
+        for i in range(1, n - length + 2):
+            j = i + length - 1
+            dp[i][j] = sys.maxsize
+            for k in range(i, j):
+                cost = dp[i][k] + dp[k+1][j] + p[i-1] * p[k] * p[j]
+                if cost < dp[i][j]:
+                    dp[i][j] = cost
+
+    return dp[1][n]
+
+N = int(input())
+hang = [list(map(int, input().split())) for _ in range(N)]
+
+p = [hang[0][0]] + [sizes[1] for sizes in hang]
+
+print(matrix_chain_order(p))
