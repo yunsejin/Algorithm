e84bcc77ad53392373600a9c3f14558e7c978dce
yunsejin <100738553+yunsejin@users.noreply.github.com>
2024-01-30 20:20:41 +0900

    [Bronze I] Title: 평균은 넘겠지, Time: 120 ms, Memory: 110404 KB -BaekjoonHub

--- 백준/Bronze/4344. 평균은 넘겠지/평균은 넘겠지.py
+++ 백준/Bronze/4344. 평균은 넘겠지/평균은 넘겠지.py
@@ -1,1 +1,16 @@
-exec('n,*l=map(int,input().split());m=sum(l)/n;print("%.3f%%"%(sum(i>m for i in l)/n*100));'*int(input()))
+a = int(input())
+
+for _ in range(a):
+    b = list(map(int, input().split()))
+    total_scores = sum(b[1:])
+    average_score = total_scores / b[0]
+    
+    above_average_count = 0
+    for score in b[1:]:
+        if score > average_score:
+            above_average_count += 1
+    
+    percentage_above_average = (above_average_count / b[0]) * 100
+    formatted_percentage = '{:.3f}%'.format(percentage_above_average)
+    
+    print(formatted_percentage)
