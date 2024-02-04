2abe71e2b9d76a142c2adee0fb1fb5deab8e47bd
yunsejin <100738553+yunsejin@users.noreply.github.com>
2024-02-04 14:23:09 +0900

    [Bronze V] Title: 카드 게임, Time: 0 ms, Memory: 1112 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/5522. 카드 게임/카드 게임.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#define _CRT_SECURE_NO_WARNINGS
+int main(void) {
+    int a, b, c, d, e;
+    scanf("%d\n%d\n%d\n%d\n%d", &a, &b, &c, &d, &e);
+    printf("%d", a + b + c + d + e);
+}
