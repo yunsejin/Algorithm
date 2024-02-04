31da7dda72a21380722e8d3207ffb1ceaf93d0c2
yunsejin <100738553+yunsejin@users.noreply.github.com>
2024-02-04 14:51:22 +0900

    [Bronze III] Title: 포인터 공부, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/25703. 포인터 공부/포인터 공부.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#define _CRT_SECURE_NO_WARNINGS
+int main(void) {
+    int N;
+    scanf("%d", &N);
+    printf("int a;\n");
+    printf("int *ptr = &a;\n");
+    if (N > 1){
+        printf("int **ptr2 = &ptr;\n"); 
+    }
+    for(int i = 3; i <= N; i++) {
+        printf("int ");
+        for(int j = 1; j <= i; j++) {
+            printf("*");
+        }
+        printf("ptr%d = &ptr%d;\n", i, i - 1 ? i - 1 : 0);
+    }
+}
