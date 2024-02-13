2865d81f1a6b549bcf610d8af9b74a44b7fb939b
yunsejin <100738553+yunsejin@users.noreply.github.com>
2024-02-13 17:03:13 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 3120 ms, Memory: 126176 KB -BaekjoonHub

---
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.py
@@ -0,0 +1,13 @@
+import sys
+input=sys.stdin.readline
+
+n = int(input())
+arr = [0] * 10001
+
+for _ in range(n):
+    arr[int(input())] += 1
+
+for i in range(10001):
+    if arr[i]:
+        for _ in range(arr[i]):
+            print(i)
