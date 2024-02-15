d0479222d97524f4eb3057a4ad7b423ba8f98919
yunsejin <100738553+yunsejin@users.noreply.github.com>
2024-02-15 13:03:28 +0900

    [Bronze I] Title: 팰린드롬수, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

--- 백준/Bronze I/1259. 팰린드롬수/팰린드롬수.c
+++ 백준/Bronze I/1259. 팰린드롬수/팰린드롬수.c
@@ -1,15 +1,6 @@
 #include <stdio.h>
 #include <string.h>
 
-void reverseString(char *str) {
-    int length = strlen(str);
-    for (int i = 0; i < length / 2; i++) {
-        char temp = str[i];
-        str[i] = str[length - i - 1];
-        str[length - i - 1] = temp;
-    }
-}
-
 int isPalindrome(char *str) {
     int length = strlen(str);
     for (int i = 0; i < length / 2; i++) {
