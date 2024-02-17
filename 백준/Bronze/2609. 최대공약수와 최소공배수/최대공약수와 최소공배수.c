887f524e83e7b886fff5df9c607fc160229ecfb6
yunsejin <100738553+yunsejin@users.noreply.github.com>
2024-02-17 11:06:35 +0900

    [Bronze I] Title: 최대공약수와 최소공배수, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.c
@@ -0,0 +1,17 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main() {
+	int a, b;
+	int x, y;
+		
+	scanf("%d %d", &a, &b);
+
+	for (int i = 1; i <= a && i <= b; i++) {
+		if (a % i == 0 && b % i == 0)
+			x = i;	
+	}
+	
+	printf("%d\n", x);
+	printf("%d", a * b / x);
+}
