706ad5ae1eeca00b4a5bf8d36a5b82e9f44da0a2
yunsejin <100738553+yunsejin@users.noreply.github.com>
2024-02-18 17:44:55 +0900

    [level 0] Title: 코드 처리하기, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/181932. 코드 처리하기/코드 처리하기.py
+++ 프로그래머스/0/181932. 코드 처리하기/코드 처리하기.py
@@ -1,22 +1,19 @@
 def solution(code):
     mode = 0
-    ret = ""
-    
-    for idx in range(len(code)):
+    a = []
+    for i in range(len(code)):
         if mode == 0:
-            if code[idx] != "1":
-                if idx % 2 == 0:
-                    ret += code[idx]
+            if code[i] != '1':
+                if i % 2 == 0:
+                    a.append(code[i])
             else:
                 mode = 1
         else:
-            if code[idx] != "1":
-                if idx % 2 == 1:
-                    ret += code[idx]
+            if code[i] != '1':
+                if i % 2 == 1:
+                    a.append(code[i])
             else:
                 mode = 0
-    
-    if ret == "":
-        return "EMPTY"
-    else:
-        return ret
+    if not a:
+        return 'EMPTY'
+    return ''.join(a)
