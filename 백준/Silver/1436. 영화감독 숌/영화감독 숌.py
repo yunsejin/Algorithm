72c16edc768649f53bc59cf19c4718bff95b8a30
yunsejin <100738553+yunsejin@users.noreply.github.com>
2024-02-24 13:36:08 +0900

    [Silver V] Title: 영화감독 숌, Time: 196 ms, Memory: 110272 KB -BaekjoonHub

---
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.py
@@ -0,0 +1,16 @@
+def main():
+    n = int(input())
+    cnt = 0
+    result = 666
+    
+    while True:
+        if '666' in str(result):
+            cnt += 1
+    
+        if cnt == n:
+            break
+        result += 1
+    
+    print(result)
+if __name__ == "__main__":
+    main()
