97aa7c0d0ab9891ef815de31fd686450ee0ee1eb
yunsejin <100738553+yunsejin@users.noreply.github.com>
2024-03-01 13:12:53 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.00 ms, Memory: 10 MB -BaekjoonHub

---
+++ 프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.py
@@ -0,0 +1,8 @@
+def solution(price):
+    if price >= 500000:
+        price -= price * 20 / 100
+    elif price >= 300000:
+        price -= price * 10 / 100
+    elif price >= 100000:
+        price -= price * 5 / 100
+    return int(price)
