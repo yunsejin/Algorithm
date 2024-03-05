e79e8a2dd51cc33d065afbc7c0b884aa0966550e
yunsejin <100738553+yunsejin@users.noreply.github.com>
2024-03-05 19:34:36 +0900

    [level 0] Title: 암호 해독, Time: 0.07 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/120892. 암호 해독/암호 해독.py
+++ 프로그래머스/0/120892. 암호 해독/암호 해독.py
@@ -1,6 +1,13 @@
 def solution(cipher, code):
-    answer = ''
-    for i in range(code,len(cipher)+1):
-        if i % code == 0:
-            answer += cipher[i-1]
-    return answer
+    from collections import deque
+    answer = deque()
+    a = list(cipher)
+    if code == 1:
+        return cipher
+    elif code == 0:
+        return ''
+    else:
+        for i in range(1 , len(a)):
+            if (i+1) % code == 0:
+                answer.append(a[i])
+        return ''.join(answer)
