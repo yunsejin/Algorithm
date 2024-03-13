562bb78f96f1dd70b3d94d0aabd5e985b8b42ae0
yunsejin <100738553+yunsejin@users.noreply.github.com>
2024-03-13 14:11:18 +0900

    [Silver II] Title: 마인크래프트, Time: 544 ms, Memory: 121796 KB -BaekjoonHub

---
+++ 백준/Silver/18111. 마인크래프트/마인크래프트.py
@@ -0,0 +1,22 @@
+import sys
+input = sys.stdin.readline
+N, M, B = map(int, input().split())
+ground = []
+for _ in range(N):
+    ground.extend(map(int, input().split()))
+
+time = [0 for i in range(257)]
+height = 0
+for g in range(257):
+    block = B
+    for i in ground:
+        if i <= g:
+            time[g] += g - i
+            block -= g - i
+        else:
+            time[g] += 2 * (i - g)
+            block += i - g
+    if block >= 0 and time[g] <= time[height]:
+        height = g
+
+print(time[height], height)
