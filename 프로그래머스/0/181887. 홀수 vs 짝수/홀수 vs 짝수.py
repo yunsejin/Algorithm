69513a0853c5e6bc0a95e98bb6ed15f8b30a5a56
yunsejin <100738553+yunsejin@users.noreply.github.com>
2024-03-16 19:46:17 +0900

    [level 0] Title: 홀수 vs 짝수, Time: 0.00 ms, Memory: 10.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181887. 홀수 vs 짝수/홀수 vs 짝수.py
@@ -0,0 +1,16 @@
+def solution(num_list):
+    answer = 0
+    a = 0
+    b = 0
+
+    for i in range(len(num_list)):
+        if i % 2 == 0:
+            a += num_list[i]
+        else:
+            b += num_list[i]
+    
+    if a > b:
+        answer = a
+    else:
+        answer = b
+    return answer
