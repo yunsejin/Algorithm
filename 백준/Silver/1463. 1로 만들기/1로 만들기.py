d04c09ccde3b3af20f4c184c10374af8d7b10435
yunsejin <100738553+yunsejin@users.noreply.github.com>
2024-03-21 15:14:22 +0900

    [Silver III] Title: 1로 만들기, Time: 144 ms, Memory: 118408 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.py
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.py
@@ -1,11 +1,18 @@
-a = int(input())
-dp = [0,0,0,0,0,0,0,0,0]
+dp = [0] * (10**6+1)
+dp[0] = 0
 dp[1] = 0
 dp[2] = 1
 dp[3] = 1
-i = 3
-while i <= a:
-    i += 1
-    dp.append(0)
+dp[4] = 2
+dp[5] = 3
+dp[6] = 2
+dp[7] = 3
+dp[8] = 3
+dp[9] = 2
+dp[10] = 3
+for i in range(11, 10**6+1):
     dp[i] = min(dp[i-1], dp[i//2] if i % 2 == 0 else 10**6, dp[i//3] if i % 3 == 0 else 10**6) + 1    
-print(dp[a])
+import sys
+input = sys.stdin.readline
+n = int(input())
+print(dp[n])
