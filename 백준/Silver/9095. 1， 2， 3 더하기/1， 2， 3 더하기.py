84f8c475284769f558deb5f6f587dd894102b88d
yunsejin <100738553+yunsejin@users.noreply.github.com>
2024-03-22 12:39:02 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 64 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.py
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.py
@@ -8,12 +8,7 @@ for i in range(n):
     dp[1] = 1
     dp[2] = 2
     dp[3] = 4
-    dp[4] = 7
-    dp[5] = 13
-    dp[6] = 24
-    dp[7] = 44
-    dp[8] = 81
-    dp[9] = 149
-    dp[10] = 274
-    dp[11] = 504
+    if a > 3:
+        for i in range(4, a+1):
+            dp[i] = dp[i-1] + dp[i-2] + dp[i-3]
     print(dp[a])
