3db45ed1e0bec47fcf4965c17d9fcf8b5315342f
yunsejin <100738553+yunsejin@users.noreply.github.com>
2024-04-03 16:19:26 +0900

    [level 0] Title: 커피 심부름, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181837. 커피 심부름/커피 심부름.py
@@ -0,0 +1,15 @@
+def solution(order):
+    answer = 0
+    d = {
+        "iceamericano": 4500, "americanoice": 4500,
+        "hotamericano": 4500, "americanohot": 4500,
+        "icecafelatte": 5000, "cafelatteice": 5000,
+        "hotcafelatte": 5000, "cafelattehot": 5000,
+        "americano": 4500, "cafelatte": 5000,
+        "anything": 4500
+    }
+
+    for i in order:
+        if i in d:
+            answer += d[i]
+    return answer
