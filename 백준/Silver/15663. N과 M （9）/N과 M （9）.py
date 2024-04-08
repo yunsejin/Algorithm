0f9d65f1caa33898ebb4b0903665bad57267ffc3
yunsejin <100738553+yunsejin@users.noreply.github.com>
2024-04-08 22:11:20 +0900

    [Silver II] Title: N과 M (9), Time: 92 ms, Memory: 35732 KB -BaekjoonHub

--- 백준/Silver/15663. N과 M （9）/N과 M （9）.py
+++ 백준/Silver/15663. N과 M （9）/N과 M （9）.py
@@ -1,19 +1,9 @@
-def permute_unique(nums, path, result):
-    if len(path) == M:
-        result.append(path[:])
-        return
-    
-    for i in range(len(nums)):
-        if i > 0 and nums[i] == nums[i-1]:
-            continue
-        path.append(nums[i])
-        permute_unique(nums[:i] + nums[i+1:], path, result)
-        path.pop()
+from itertools import permutations
 
 N, M = map(int, input().split())
 numbers = sorted(list(map(int, input().split())))
-result = []
-permute_unique(numbers, [], result)
 
-for seq in result:
+result_set = set(permutations(numbers, M))
+
+for seq in sorted(result_set):
     print(' '.join(map(str, seq)))
