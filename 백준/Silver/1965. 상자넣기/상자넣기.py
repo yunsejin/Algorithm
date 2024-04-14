69c0b5660d52fb67dadd30df07647fcf0f3d52e0
yunsejin <100738553+yunsejin@users.noreply.github.com>
2024-04-14 16:31:54 +0900

    [Silver II] Title: 상자넣기, Time: 220 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1965. 상자넣기/상자넣기.py
+++ 백준/Silver/1965. 상자넣기/상자넣기.py
@@ -1,12 +1,9 @@
 n = int(input())
 box = list(map(int, input().split()))
 dp = [0] * 1000
-if len(box) == 1:
-    print(1)
-else:
-    for i in range(n):
-        dp[i] = 1
-        for j in range(i):
-            if box[i] > box[j]:
-                dp[i] = max(dp[i], dp[j] + 1)
-    print(max(dp))
+for i in range(n):
+    dp[i] = 1
+    for j in range(i):
+        if box[i] > box[j]:
+            dp[i] = max(dp[i], dp[j] + 1)
+print(max(dp))
